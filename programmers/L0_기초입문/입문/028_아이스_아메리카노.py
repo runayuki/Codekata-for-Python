465b73c90dd7229d6338ab7cb5854ffdd0c55c47
runayuki <subin8640@gmail.com>
2026-01-20 09:47:18 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,14 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 박수빈
+# 작성일: 2026. 01. 20. 09:47:17
+
+def solution(money):
+    answer = []
+    cup = money // 5500
+    remain = money - (cup * 5500)
+    answer.append(cup)
+    answer.append(remain)
+    return answer
