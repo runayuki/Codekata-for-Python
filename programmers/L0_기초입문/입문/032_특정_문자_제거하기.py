8c8d293d324d93648c9910fbd6d787046e0c93f6
runayuki <subin8640@gmail.com>
2026-01-21 09:15:48 +0900

    [프로그래머스] L0 - 특정 문자 제거하기 (#32)

---
+++ programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
@@ -0,0 +1,10 @@
+# 특정 문자 제거하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120826
+# 알고리즘: 기초
+# 작성자: 박수빈
+# 작성일: 2026. 01. 21. 09:15:48
+
+def solution(my_string, letter):
+    answer = my_string.replace(letter, '')
+    return answer
